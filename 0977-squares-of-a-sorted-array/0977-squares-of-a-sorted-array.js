3669a0efc8b6b6576d7af3ae15d7473c85cf1284
MUHAMMED RASHIK VP <rrasik606@gmail.com>
2026-07-20 12:08:21 +0530

    Time: 9 ms (38.34%), Space: 62.6 MB (42.13%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,7 +3,6 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-let sqnums=nums.map((num)=> num*num)
-let sort=sqnums.sort((a,b)=> a-b)
-return sort
+let sortedArray=nums.map((num)=> num*num).sort((a,b)=> a-b)
+return sortedArray
 };
